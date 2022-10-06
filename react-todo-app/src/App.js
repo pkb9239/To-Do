9105b7e441a1e4f4a322108ae988f350511b504c
Kyungbin <pkb8839@naver.com>
2022-10-06 19:02:26 +0900

    Form 부분을 위한 컴포넌트 생성하기

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 import List from "./components/List";
 
 export default function App() {
   const [todoData, setToData] = useState([]);
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,31 +17,19 @@ export default function App() {
     // 원래 있던 할 일에 새로운 할 일 더해주기
     setToData(prev => [...prev, newToDo]);
     setValue("");
-  }; 
-    return (
-      <div className="container">
-        <div className="todoBlock">
-          <div className="title">
-            <h1>할 일 목록</h1>
-          </div>
-          <List todoData={todoData} setToData={setToData} />
-          <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="value"
-              style={{ flex: '10', padding: '5px' }}
-              placeholder="해야 할 일을 입력하세요."
-              value={value}
-              onChange={handleChange}
-            />
-            <input
-              type="submit"
-              value="입력"
-              className="btn"
-              style={{ flex: '1' }}
-            />
-          </form>
+  };
+
+  return (
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
         </div>
+        
+        <List todoData={todoData} setToData={setToData} />
+        
+        <Form value={value} setValue={setValue} handleSubmit={handleSubmit} />
       </div>
-    )
-  }
+    </div>
+  )
+}
