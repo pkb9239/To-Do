736850511128f4dba15be0a7bb3b97266f640371
Kyungbin <pkb8839@naver.com>
2022-10-12 14:35:20 +0900

    List 컴포넌트 생성하기

--- react-todo-app/src/components/List.js
+++ react-todo-app/src/components/List.js
@@ -1,19 +1,8 @@
 import React from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
+import Lists from './Lists';
 export default function List({ todoData, setToData }) {
-  const hadleCheckBox = (id) => {
-    let newToDoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setToData(newToDoData);
-  }
-  const handleClick = (id) => {
-    let newToDoData = todoData.filter((data) => data.id !== id);
-    setToData(newToDoData);
-  };
+
   const handleEnd = (result) => {
     // result 매개변수에는 source 항목 및 대상 위치와 같은 드래그 이벤트에 대한 정보가 포함된다.
     if (!result.destination) return;
@@ -42,23 +31,16 @@ export default function List({ todoData, setToData }) {
                   index={index}
                 >
                   {(provided, snapshot) => (
-                    <div
-                      key={data.id} {...provided.draggableProps}
-                      ref={provided.innerRef} {...provided.dragHandleProps}
-                      className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded`}
-                    >
-                      <div className='items-center'>
-                        <input
-                          type="checkbox"
-                          defaultChecked={false}
-                          onChange={() => hadleCheckBox(data.id)}
-                        />{" "}
-                        <span className={data.completed ? 'line-through' : undefined}>{data.title}</span>
-                      </div>
-                      <div className='items-center'>
-                        <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
-                      </div>
-                    </div>
+                    <Lists 
+                    key={data.id}
+                    id={data.id}
+                    title={data.title}
+                    compeleted={data.compeleted}
+                    todoData={todoData}
+                    setToData={setToData}
+                    provided={provided}
+                    snapshot={snapshot} 
+                    />
                   )}
                 </Draggable>
               )}
